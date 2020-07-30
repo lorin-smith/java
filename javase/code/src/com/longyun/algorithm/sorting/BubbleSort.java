e9ed9c111b6ab30cc605cffd2f0ad8be6151abfc
lorin-smith <lorin-smith@outlook.com>
2020-07-30 22:48:29 +0800

    冒泡和选择添加注释

--- javase/code/src/com/longyun/algorithm/sorting/BubbleSort.java
+++ javase/code/src/com/longyun/algorithm/sorting/BubbleSort.java
@@ -2,11 +2,13 @@ package com.longyun.algorithm.sorting;
 
 /**
  * 冒泡排序
+ * 1、比较相邻的两个值
+ * 2、如果大小关系不对，交换两个值
+ * 3、重复1和2，直到到达数组的末尾
+ * 4、数组的末尾向前移动一位，重复1、2、3，直到数组的末尾为1
  */
 public class BubbleSort {
     /**
-     * 对传递过来的整形数组进行冒泡排序
-     *
      * @param arr 整形数组
      */
     public static void sort(int[] arr) {
--- javase/code/src/com/longyun/algorithm/sorting/SelectSort.java
+++ javase/code/src/com/longyun/algorithm/sorting/SelectSort.java
@@ -1,15 +1,37 @@
 package com.longyun.algorithm.sorting;
 
+/**
+ * 选择排序
+ * 首先从未排序的序列中找到最小（大）值放到首位，然后，从剩余的未排序中找最小（大）值放在已排序的末尾，
+ * 以此类推，直到序列的尾部。
+ * 注意：
+ *     每次循环只交换一次值，记录的是下标的值
+ */
 public class SelectSort {
+    /**
+     * @param arr 整形数组
+     */
     public static void sort(int[] arr) {
+//        可以实现结果，但交换次数太多
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            for (int j = i + 1; j < arr.length; j++) {
+//                if (arr[i] > arr[j]) {
+//                    int t = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = t;
+//                }
+//            }
+//        }
         for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int t = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = t;
+                if (arr[index] > arr[j]) {
+                    index = j;
                 }
             }
+            int t = arr[i];
+            arr[i] = arr[index];
+            arr[index] = t;
         }
     }
 }
